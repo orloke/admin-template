66fc87dca0cb17d2b9ba79ad24e1e5f43c682608
Junior Dering <juniordering@hotmail.com>
2022-08-04 19:11:20 -0400

    correct name

--- src/components/ButtonDarkMode/ButtonDarkMode.spec.tsx
+++ src/components/ButtonDarkMode/ButtonDarkMode.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import ButtonDarkMode from './ButtonDrakMode'
+import ButtonDarkMode from './ButtonDarkMode'
 import userEvent  from '@testing-library/user-event'
 
 describe('ButtonDarkMode component', () => {
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import useAppData from "../data/hooks/useAppData";
 import DefenderRoute from "./auth/DefenderRoute";
 import Avatar from "./Avatar/Avatar";
-import ButtonDarkMode from "./ButtonDarkMode/ButtonDrakMode";
+import ButtonDarkMode from "./ButtonDarkMode/ButtonDarkMode";
 import Content from "./Content/Content";
 import { IconMenu } from "./icons";
 import Sidebar from "./Sidebar/Sidebar";
