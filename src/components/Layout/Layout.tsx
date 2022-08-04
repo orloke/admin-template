660bb957364239a5533dc8ea15e0a5593898181e
Junior Dering <juniordering@hotmail.com>
2022-08-04 19:38:18 -0400

    improving code

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,52 +8,51 @@ import { IconMenu } from "../icons";
 import Sidebar from "../Sidebar/Sidebar";
 import TopBar from "../TopBar/TopBar";
 
-interface Props{
-    title: string;
-    caption: string;
-    children: React.ReactNode
+interface Props {
+  title: string;
+  caption: string;
+  children: React.ReactNode;
 }
 
 const Layout = (props: Props) => {
+  const [open, setOpen] = useState(true);
 
-    const [open, setOpen] = useState(true)
-
-    const {tema, changeThema} = useAppData()
+  const { tema, changeThema } = useAppData();
 
   return (
     <DefenderRoute>
-        <div className = {` ${tema} flex h-screen`}>
-            
-                <div className={`flex flex-col items-end`}>
-                    <div>
-                        <button 
-                            className={`text-3xl absolute ml-2 mt-2 dark:text-white `}
-                            onClick={() => setOpen(!open)} 
-                        >
-                                {IconMenu}                    
-                        </button>
-                    </div>
-                    <div className={`${open?'':'-ml-20 '}  ease-in-out duration-300`}>
-                        <Sidebar/>
-                    </div>
-
-                </div>
-
-            <div className="flex flex-col w-full bg-gray-400 dark:bg-gray-800 items-end" >
-                <div className="flex mt-2" >
-                    <ButtonDarkMode thema={tema} changeThema={changeThema} />
-                    <Avatar className="h-8 w-8 rounded-full cursor-pointer mx-8" />
-                </div>
-                <div className={`flex flex-col h-screen w-full pt-20 sm:pt-7 pl-2   ${open?'':'pl-2'} `} >
-                    <TopBar title={props.title} caption={props.caption} />
-                    <Content>
-                        {props.children}
-                    </Content>
-                </div>
-            </div>
+      <div className={` ${tema} flex h-screen`}>
+        <div className={`flex flex-col items-end`}>
+          <div>
+            <button
+              className={`text-3xl absolute ml-2 mt-2 dark:text-white `}
+              onClick={() => setOpen(!open)}
+            >
+              {IconMenu}
+            </button>
+          </div>
+          <div className={`${open ? "" : "-ml-20 "}  ease-in-out duration-300`}>
+            <Sidebar />
+          </div>
         </div>
+
+        <div className="flex flex-col w-full bg-gray-400 dark:bg-gray-800 items-end">
+          <div className="flex mt-2">
+            <ButtonDarkMode thema={tema} changeThema={changeThema} />
+            <Avatar className="h-8 w-8 rounded-full cursor-pointer mx-8" />
+          </div>
+          <div
+            className={`flex flex-col h-screen w-full pt-20 sm:pt-7 pl-2   ${
+              open ? "" : "pl-2"
+            } `}
+          >
+            <TopBar title={props.title} caption={props.caption} />
+            <Content>{props.children}</Content>
+          </div>
+        </div>
+      </div>
     </DefenderRoute>
-  )
-}
+  );
+};
 
 export default Layout;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,32 +1,42 @@
-import useAuth from '../../data/hooks/useAuth';
-import { IconHouse, IconLogout, IconNotification, IconSettings } from '../icons';
-import Logo from '../Logo/Logo';
-import MenuItem from '../MenuItem/MenuItem';
+import useAuth from "../../data/hooks/useAuth";
+import {
+  IconHouse,
+  IconLogout,
+  IconNotification,
+  IconSettings,
+} from "../icons";
+import Logo from "../Logo/Logo";
+import MenuItem from "../MenuItem/MenuItem";
 
 const Sidebar = () => {
-
-  const {logout} = useAuth()
+  const { logout } = useAuth();
 
   return (
-
-        <aside className='flex flex-col h-screen dark:bg-gray-900' >
-          <div className={`h-20 w-20 bg-gradient-to-r from-indigo-500 via-blue-700 to-purple-800 flex flex-col items-center justify-center`} >
-            <Logo/>
-          </div>
-          <ul className='flex-grow' >
-              <MenuItem url='/' text='Home' icon= {IconHouse} />
-              <MenuItem url='/settings' text='Ajustes' icon= {IconSettings} />
-              <MenuItem url='/notification' text='Notificações' icon= {IconNotification} />
-          </ul>
-          <ul>
-              <MenuItem 
-                onClick={logout} 
-                text='Sair' icon= {IconLogout} 
-                className = 'text-red-500 dark:text-red-400 hover:bg-red-500 dark:hover:text-white hover:text-white ' />
-          </ul>
-        </aside>
-
-  )
-}
+    <aside className="flex flex-col h-screen dark:bg-gray-900">
+      <div
+        className={`h-20 w-20 bg-gradient-to-r from-indigo-500 via-blue-700 to-purple-800 flex flex-col items-center justify-center`}
+      >
+        <Logo />
+      </div>
+      <ul className="flex-grow">
+        <MenuItem url="/" text="Home" icon={IconHouse} />
+        <MenuItem url="/settings" text="Ajustes" icon={IconSettings} />
+        <MenuItem
+          url="/notification"
+          text="Notificações"
+          icon={IconNotification}
+        />
+      </ul>
+      <ul>
+        <MenuItem
+          onClick={logout}
+          text="Sair"
+          icon={IconLogout}
+          className="text-red-500 dark:text-red-400 hover:bg-red-500 dark:hover:text-white hover:text-white "
+        />
+      </ul>
+    </aside>
+  );
+};
 
 export default Sidebar;
