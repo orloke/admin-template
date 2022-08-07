9c8cdd8d1df202614c921f97ae8e8f7a65dd02f1
Junior Dering <juniordering@hotmail.com>
2022-08-07 16:45:03 -0400

    corrigindo teste

--- src/components/TopBar/TopBar.spec.tsx
+++ src/components/TopBar/TopBar.spec.tsx
@@ -1,10 +1,10 @@
-import {  render, screen } from '@testing-library/react'
+import {  render } from '@testing-library/react'
 import TopBar from './TopBar'
 
 
 describe('TopBar component', () => {
     test('render correctly', () => {
-        const {container, getByText, getByRole} = render(<TopBar title='Teste' caption='Testando o layout'/>)
+        const { getByText } = render(<TopBar title='Teste' caption='Testando o layout'/>)
         expect(getByText('Teste')).toBeInTheDocument()
         expect(getByText('Testando o layout')).toBeInTheDocument()
     })
