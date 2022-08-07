166354cfa075e49d21b9959f686b1a7047f4ca4e
Junior Dering <juniordering@hotmail.com>
2022-08-07 16:45:15 -0400

    tirando parte desnecessaria

--- src/pages/notification.tsx
+++ src/pages/notification.tsx
@@ -1,13 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/Layout/Layout'
 import useAppData from '../data/hooks/useAppData'
-import styles from '../styles/Home.module.css'
+
 
 const Notification = () => {
-
-  const {changeThema} = useAppData()
 
   return (
     <Layout title='Notificações' caption='Testando o layout' >
